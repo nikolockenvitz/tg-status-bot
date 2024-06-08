7dd42bf95c3ee47f07b21952a1d6c2231674dec3
Niko Lockenvitz <nl@nikolockenvitz.de>
2024-06-08 10:40:34 +0200

    update discount scanner kaufland

--- actions/discount-scanner-kaufland.ts
+++ actions/discount-scanner-kaufland.ts
@@ -7,12 +7,10 @@ import { IWeeklyExecutionTime } from "../types";
 const config = require("./general/config-import");
 
 export default class KauflandDiscountScanner extends AbstractAction {
-  protected baseUrl = "https://filiale.kaufland.de";
   protected config: any;
   protected weeklyExecutionTime: IWeeklyExecutionTime;
   protected searchTerms: string[] = [];
   protected ignoreSearchTerms: string[] = [];
-  protected ignoreUrlPaths: string[] = [];
   protected cookies: string = "";
   protected description: string = "";
 
@@ -30,7 +28,6 @@ export default class KauflandDiscountScanner extends AbstractAction {
     };
     this.searchTerms = this.config?.searchTerms;
     this.ignoreSearchTerms = this.config?.ignoreSearchTerms;
-    this.ignoreUrlPaths = this.config?.ignoreUrlPaths;
     this.cookies = this.config?.cookies;
     this.description =
       this.config?.description ||
@@ -58,7 +55,7 @@ export default class KauflandDiscountScanner extends AbstractAction {
   async run(data: any, bot: TelegramBot): Promise<boolean> {
     try {
       const t0 = Date.now();
-      const urls = await getDiscountUrls(this.baseUrl, this.ignoreUrlPaths);
+      const urls = getDiscountUrls();
       let errors = 0;
       const discounts: IDiscount[] = ([] as IDiscount[]).concat(
         ...(
@@ -110,14 +107,14 @@ export default class KauflandDiscountScanner extends AbstractAction {
           continue;
         }
         if (
-          discount.valid.from !== prevSectionValidity.from ||
-          discount.valid.to !== prevSectionValidity.to
+          discount.valid.fromIso !== prevSectionValidity.from ||
+          discount.valid.toIso !== prevSectionValidity.to
         ) {
-          prevSectionValidity.from = discount.valid.from;
-          prevSectionValidity.to = discount.valid.to;
+          prevSectionValidity.from = discount.valid.fromIso;
+          prevSectionValidity.to = discount.valid.toIso;
           message += validityFromToToMarkdown(
-            discount.valid.from,
-            discount.valid.to
+            discount.valid.fromIso,
+            discount.valid.toIso
           );
         }
         message += discountToMarkdown(discount);
@@ -142,8 +139,8 @@ export default class KauflandDiscountScanner extends AbstractAction {
 }
 
 interface IValidFromTo {
-  from: string;
-  to: string;
+  fromIso: string;
+  toIso: string;
   fromUnixTimestamp: number;
   toUnixTimestamp: number;
 }
@@ -153,117 +150,18 @@ interface IDiscount {
   title: string | undefined;
   quantity: string;
   basicPrice: string | undefined;
-  disclaimer: string | undefined;
   discount: string | undefined;
   oldPrice: string;
   price: string;
   valid: IValidFromTo;
 }
 
-async function getDiscountUrls(
-  baseUrl: string,
-  ignoreUrlPaths: string[]
-): Promise<string[]> {
-  const html = await fetch("GET", baseUrl + "/angebote/aktuelle-woche.html");
-
-  const regexNavUrls = new RegExp(
-    `<a class="o-navigation-main__link o-navigation-main__link--level-(?<level>\\d+)\\s*" role="menuitem" href="(?<url>[^"]*)"`,
-    "g"
-  );
-  const navUrls: Array<{ level: number; url: string }> = [];
-  let matchUrl;
-  while ((matchUrl = regexNavUrls.exec(html))) {
-    navUrls.push({
-      level: Number(matchUrl.groups.level),
-      url: matchUrl.groups.url,
-    });
-  }
-
-  // links are in tree-structure, only leaf nodes for discounts ("/angebote/...") are interesting
-  const discountUrls: string[] = [];
-  for (let i = 0; i < navUrls.length; i++) {
-    if (
-      (i === navUrls.length - 1 || navUrls[i] <= navUrls[i + 1]) &&
-      navUrls[i].url.startsWith("/angebote/") &&
-      !ignoreUrlPaths.includes(navUrls[i].url)
-    ) {
-      discountUrls.push(baseUrl + navUrls[i].url);
-    }
-  }
-
-  for (const furtherDiscountUrl of await findFurtherDiscountUrlsOnSubPagesSideAccordion(
-    discountUrls,
-    baseUrl,
-    ignoreUrlPaths
-  )) {
-    discountUrls.push(furtherDiscountUrl);
-  }
-  for (const furtherDiscountUrl of await findFurtherDiscountUrlsOnSubPagesSeeAll(
-    discountUrls,
-    baseUrl,
-    ignoreUrlPaths
-  )) {
-    discountUrls.push(furtherDiscountUrl);
-  }
-
-  return discountUrls;
-}
-
-async function findFurtherDiscountUrlsOnSubPagesSideAccordion(
-  discountUrls: string[],
-  baseUrl: string,
-  ignoreUrlPaths: string[]
-): Promise<string[]> {
-  const furtherDiscountUrls: string[] = [];
-
-  // for some discounts, there is a accordion on the page with further pages
-  for (const discountUrl of discountUrls) {
-    if (!discountUrl.endsWith("-woche.html")) continue;
-
-    const html = await fetch("GET", discountUrl);
-    const regexShowAllUrls = new RegExp(
-      `<li class="m-accordion__item m-accordion__item--level-2( m-accordion__link--active|)">` +
-        `\\s*<a href="(?<url>[^"]*)" class="m-accordion__link">`,
-      "g"
-    );
-    let matchUrl;
-    while ((matchUrl = regexShowAllUrls.exec(html))) {
-      const url = matchUrl.groups.url;
-      if (url.startsWith("/angebote/") && !ignoreUrlPaths.includes(url)) {
-        furtherDiscountUrls.push(baseUrl + url);
-      }
-    }
-  }
-
-  return furtherDiscountUrls;
-}
-
-async function findFurtherDiscountUrlsOnSubPagesSeeAll(
-  discountUrls: string[],
-  baseUrl: string,
-  ignoreUrlPaths: string[]
-): Promise<string[]> {
-  const furtherDiscountUrls: string[] = [];
-
-  // for some discounts, there is a "see all" link on the page
-  for (const discountUrl of discountUrls) {
-    if (!discountUrl.endsWith("-woche.html")) continue;
-
-    const html = await fetch("GET", discountUrl);
-    const regexShowAllUrls = new RegExp(
-      `<a class=" a-link a-link--icon-arrow a-link--underlined" href="(?<url>[^"]*)" target="_self" data-t-name="Link" title="Alle Angebote anzeigen">`,
-      "g"
-    );
-    let matchUrl;
-    while ((matchUrl = regexShowAllUrls.exec(html))) {
-      const url = matchUrl.groups.url;
-      if (url.startsWith("/angebote/") && !ignoreUrlPaths.includes(url)) {
-        furtherDiscountUrls.push(baseUrl + url);
-      }
-    }
-  }
-
-  return furtherDiscountUrls;
+function getDiscountUrls(): string[] {
+  return [
+    `https://filiale.kaufland.de/angebote/uebersicht.html?kloffer-week=current`,
+    // `https://filiale.kaufland.de/angebote/uebersicht.html?kloffer-week=next`,
+    // next week is already included in the JSON we parse
+  ];
 }
 
 async function getDiscounts(
@@ -275,82 +173,79 @@ async function getDiscounts(
     headers: { Cookie: cookies },
   });
 
-  const regexValidFromTo = new RegExp(
-    `<h\\d>Gültig vom (\\d{2}\\.\\d{2}\\.\\d{4}) bis (\\d{2}\\.\\d{2}\\.\\d{4})</h\\d>`
-  );
-  const matchValidFromTo = regexValidFromTo.exec(html);
-  if (matchValidFromTo === null) {
-    if (html.includes("liegen keine Angebote für die nächste Woche vor")) {
-      return { discounts: [], errors };
+  try {
+    const rawJsonStr =
+      "{" +
+      html.split(`= {"component":"OfferTemplate",`)[1].split("</script>")[0];
+    const jsonDiscountData = JSON.parse(rawJsonStr).props.offerData;
+
+    const discounts: IDiscount[] = [];
+    for (const offer of jsonDiscountData.loyalty.offers) {
+      const discount = parseOffer(offer);
+      if (discount) discounts.push(discount);
+      else errors += 1;
     }
-    throw new Error(`Failed to parse date valid_from/to (${url})`);
-  }
-  const valid: IValidFromTo = {
-    from: matchValidFromTo[1],
-    to: matchValidFromTo[2],
-    fromUnixTimestamp: dateStringToUnixTimestamp(matchValidFromTo[1]),
-    toUnixTimestamp: dateStringToUnixTimestamp(matchValidFromTo[2]),
-  };
-
-  const regexDiscount = new RegExp(
-    `<h5 class="m-offer-tile__subtitle">(?<subtitle>[^<]*)</h5>` +
-      `(\\s*<h4 class="m-offer-tile__title">\\s*(?<title>.*[^\\s])\\s*</h4>|)` +
-      `\\s*<div class="m-offer-tile__quantity">\\s*(?<quantity>.*[^\\s])\\s*</div>` +
-      `(\\s*<div class="m-offer-tile__basic-price">(?<basicPrice>[^<]*) </div>|)` +
-      `\\s*</div>\\s*</div>\\s*<div class="m-offer-tile__split">\\s*<div class="m-offer-tile__price-tiles">` +
-      `\\s*<div class="a-pricetag[^\\"]*"[^>]*>` +
-      `(\\s*<div class="a-pricetag__disclaimer">\\s*(?<disclaimer>.*[^\\s])\\s*</div>|)` +
-      `(\\s*<div class="a-pricetag__discount">\\s*(?<discount>.*[^\\s])\\s*</div>|)` +
-      `\\s*<div class="a-pricetag__price-container( |)">` +
-      `\\s*<div class="a-pricetag__old-price">` +
-      `\\s*<span class="a-pricetag__old-price a-pricetag__line-through">\\s*(?<oldPrice>.*[^\\s])\\s*</span>` +
-      `(\\s*<span class="a-pricetag__currency">([^<]*)</span>|)` +
-      `\\s*</div>` +
-      `\\s*<div class="a-pricetag__price">\\s*(?<price>[^<]*[^\\s])\\s*(<.*>)?\\s*</div>`,
-    "g"
-  );
-  const discounts: Array<IDiscount> = [];
-  let matchDiscount;
-  while ((matchDiscount = regexDiscount.exec(html))) {
-    discounts.push({
-      ...matchDiscount.groups,
-      valid,
-    });
-  }
-
-  const subtitleEntries = html.match(
-    new RegExp(
-      `<h5 class="m-offer-tile__subtitle">(?<subtitle>[^<]*)</h5>`,
-      "g"
-    )
-  );
-  if (discounts.length !== (subtitleEntries?.length || 0)) {
-    // complete regex doesn't match for certain entries; find and log them to fix it
-    for (const subtitleEntry of subtitleEntries) {
-      const subtitle = subtitleEntry.split(">")[1].split("<")[0];
-      if (!discounts.find((d) => d.subtitle === subtitle)) {
-        console.log(
-          "Discount Scanner Kaufland failed for",
-          subtitle,
-          `(${url})`
-        );
-        errors += 1;
+    for (const cycle of jsonDiscountData.cycles) {
+      for (const category of cycle.categories) {
+        for (const offer of category.offers) {
+          const discount = parseOffer(offer);
+          if (discount) discounts.push(discount);
+          else errors += 1;
+        }
       }
     }
-  }
 
-  return { discounts, errors };
+    return { discounts, errors };
+  } catch (err) {
+    errors += 1;
+    console.log("Couldn't parse discounts on", url);
+    return { discounts: [], errors };
+  }
+}
+
+function parseOffer(offer: any): IDiscount | null {
+  try {
+    const fromIso = offer.dateFrom,
+      toIso = offer.dateTo;
+    return {
+      title: offer.title ?? "",
+      subtitle: offer.subtitle ?? "",
+      valid: {
+        fromIso,
+        toIso,
+        fromUnixTimestamp: isoDateStringToUnixTimestamp(fromIso),
+        toUnixTimestamp: isoDateStringToUnixTimestamp(toIso),
+      },
+      price: (
+        offer.loyaltyFormattedPrice ??
+        offer.formattedPrice ??
+        ""
+      ).replace(/\*/g, ""),
+      quantity: offer.unit ?? "",
+      basicPrice: (
+        offer.loyaltyFormattedBasePrice ??
+        offer.basePrice ??
+        ""
+      ).replace(/\*/g, ""),
+      discount: `-${offer.loyaltyDiscount || offer.discount || 0} %`,
+      oldPrice: offer.loyaltyFormattedOldPrice ?? offer.formattedOldPrice ?? "",
+    };
+  } catch {
+    return null;
+  }
 }
 
 function getDiscountIdentifier(discount: IDiscount) {
   return `${discount.title}:${discount.subtitle}:${discount.valid}`;
 }
 
-function dateStringToUnixTimestamp(dateString: string): number {
-  const [day, month, year] = dateString.split(".");
-  return Math.floor(
-    new Date(Number(year), Number(month) - 1, Number(day)).getTime() / 1000
-  );
+function isoDateStringToUnixTimestamp(isoDateString: string): number {
+  return Math.floor(new Date(isoDateString).getTime() / 1000);
+}
+
+function isoDateStringToDDMMYYY(isoDateString: string): string {
+  const [yyyy, mm, dd] = isoDateString.split("-");
+  return `${dd}.${mm}.${yyyy}`;
 }
 
 function doesSearchTermMatchDiscount(
@@ -358,19 +253,21 @@ function doesSearchTermMatchDiscount(
   discount: IDiscount
 ): boolean {
   const searchTermLowerCase = searchTerm.toLowerCase();
-  return `${discount.subtitle} ${discount.title}`
+  return `${discount.title} ${discount.subtitle}`
     .toLowerCase()
     .includes(searchTermLowerCase);
 }
 
-function validityFromToToMarkdown(from: string, to: string) {
-  return `🗓️ ${escapeMarkdown(from)} \\- ${escapeMarkdown(to)}\n`;
+function validityFromToToMarkdown(fromIso: string, toIso: string) {
+  return `🗓️ ${escapeMarkdown(
+    isoDateStringToDDMMYYY(fromIso)
+  )} \\- ${escapeMarkdown(isoDateStringToDDMMYYY(toIso))}\n`;
 }
 
 function discountToMarkdown(discount: IDiscount): string {
   return (
-    `*${escapeMarkdown(discount.subtitle)} ${escapeMarkdown(
-      discount.title || ""
+    `*${escapeMarkdown(discount.title)} ${escapeMarkdown(
+      discount.subtitle || ""
     )}*\n` +
     `~${escapeMarkdown(discount.oldPrice)}~ ${escapeMarkdown(
       discount.price
@@ -378,9 +275,6 @@ function discountToMarkdown(discount: IDiscount): string {
     `${escapeMarkdown(discount.quantity)} ${escapeMarkdown(
       discount.basicPrice
     )}\n` +
-    `${
-      discount.disclaimer ? `⚠ ${escapeMarkdown(discount.disclaimer)}\n` : ``
-    }` +
     `\n`
   );
 }
